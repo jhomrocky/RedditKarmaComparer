961e7ac14be395ce2dc16d74edad328a152c9bd4
Joseph H <jhomrocky@gmail.com>
2018-02-01 19:43:23 -0500

    Refined code structure, added in 404 and 429 error handling

--- Main.py
+++ Main.py
@@ -5,43 +5,90 @@ notifies the user of who's post got a higher score.
 """
 
 # importing modules
+import sys
 import json
-from urllib.request import urlopen
-
-# allows user to pick 2 usernames to compare
-# user_one = input("Name: ")
-# user_two = input("Name: ")
-# user_one = user_one.lower()
-# user_two = user_two.lower()
-
-# inserts the two users into 2 variables that uses %s to fill in the username portion
-first_json = ("https://www.reddit.com/user/normalism.json") # % user_one)
-second_json = ("https://www.reddit.com/user/clockwork8.json") # % user_two)
-
-# loading all data into json representation
-first_user_info = json.load(urlopen(first_json))
-# grabbing 'data' dictionary key
-first_user_info_data = first_user_info['data']
-# going deeper, grabbing 'children' key from data dict
-first_user_info_children = first_user_info_data['children']
-# even further, grabbing the first (most recent post) index from 'children'
-first_user_children_data = first_user_info_children[0]
-# finally grabbing the text of the message
-first_user_post_body = first_user_children_data['data']['body']
-# and grabbing the karma score as well
-first_user_post_score = first_user_children_data['data']['score']
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from time import sleep
 
 
-second_user_info = json.load(urlopen(second_json))
-second_user_info_data = second_user_info['data']
-second_user_info_children = second_user_info_data['children']
-second_user_children_data = second_user_info_children[0]
-second_user_post_body = second_user_children_data['data']['body']
-second_user_post_score = second_user_children_data['data']['score']
+def check_users():
+    # allows user to pick 2 usernames to compare
+    user_one = input("Name: ")
 
-print(first_user_post_body)
-print(first_user_post_score)
+    # uses input to put username into url
+    first_json_url = ("https://www.reddit.com/user/%s.json" % user_one)
 
-print(second_user_post_body)
-print(second_user_post_score)
+    # requests url, returns error message if a username does not exist, then restarts function if name not found
+    first_username_check = Request(first_json_url)
+    try:
+        urlopen(first_username_check)
+    except HTTPError as reason:
+        if reason.code == 404:
+            print("Incorrect username. Try again.")
+            check_users()
+        # this elif is here since the error kept popping up in testing)
+        elif reason.code == 429:
+            print("Too many requests. Try again.")
+            check_users()
+    # once we know the username is real, we then load into a json object
+    first_user_info = json.load(urlopen(first_json_url))
 
+    user_two = input("Name: ")
+    second_json_url = ("https://www.reddit.com/user/%s.json" % user_two)
+    second_username_check = Request(second_json_url)
+    try:
+        urlopen(second_username_check)
+    except HTTPError as reason:
+        if reason.code == 404:
+            print("Incorrect username. Try again")
+            check_users()
+        elif reason.code == 429:
+            print("Too many requests. Try again.")
+            check_users()
+    second_user_info = json.load(urlopen(second_json_url))
+    # grabbing 'data' dictionary key
+    first_user_info_data = first_user_info['data']
+    # going deeper, grabbing 'children' key from data dict
+    first_user_info_children = first_user_info_data['children']
+    # even further, grabbing the first (most recent post) index from 'children'
+    first_user_children_data = first_user_info_children[0]
+    # finally grabbing the text of the message
+    first_user_post_body = first_user_children_data['data']['body']
+    # and grabbing the karma score as well
+    first_user_post_score = first_user_children_data['data']['score']
+
+    # for purposes of explanation I chose to do it this way, it could have been done:
+    # first_user_info_data = first_user_info['data']['children'][0]['data']['body']
+    # unfortunately, in that form the data is put as a string so the later check
+    # of which score is higher fails
+
+    # repeat of above for second user, using shorter syntax
+    second_user_post_body = second_user_info['data']['children'][0]['data']['body']
+    second_user_post_score = second_user_info['data']['children'][0]['data']['score']
+
+    # prints post body & score for both users
+    print("%s\'s post:\n%s\n" % (user_one, first_user_post_body))
+    print("%s\'s score:\n%d\n" % (user_one, first_user_post_score))
+
+    print("%s's post:\n%s\n" % (user_two, second_user_post_body))
+    print("%s's score:\n%d\n" % (user_two, second_user_post_score))
+
+    # prints different messages based on higher score
+    if first_user_post_score > second_user_post_score:
+        print("%s did better on their post." % user_one)
+    else:
+        print("%s did better on their post." % user_one)
+
+    user_retry = input("Do another set of usernames? Y for yes, N for no:\n> ")
+    user_retry = user_retry.lower()
+    if user_retry == 'y':
+        check_users()
+    else:
+        print("Take care!")
+        sleep(2)
+        sys.exit()
+
+
+# starts check
+check_users()
